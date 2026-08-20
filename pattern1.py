6d66628f6ba5bdcad4111bcc5b6b4993f1cc376d
Varshini Anumalla <varshini.anumalla@acuvate.com>
2026-08-20 13:01:29 +0530

    Another approch

--- pattern1.py
+++ pattern1.py
@@ -5,3 +5,10 @@ for i in range(1,n+1):
     for j in range(n):
         print("*",end="")
     print()
+
+
+
+#another approch 
+num=int(input("Enter n value:"))
+for i in range(n,0,-1):
+    print(i*" "+"*" * num)
